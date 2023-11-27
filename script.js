b733536b7b9e9a34284c01192676cceb23b73e1f
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-11-28 00:39:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
 
+
 	
 	
-	s1.toLowerCase();
+	let str1=s1.toLowerCase();
 let str=s2.toLowerCase();
 	
-	// alert(s1);
+	// alert(str1);
 	// alert(str);
-	let index=s1.indexOf(str);
+	let index=str1.indexOf(str);
 	return index;
 
 }
+
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
